bcb10112a007d7f5a4d2e0ae5bd2398e0b8499a4
kesavan S <143989015+kesav2807@users.noreply.github.com>
2025-10-07 11:46:14 +0530

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -5,7 +5,7 @@ export const products = [
     price: 150,
     category: "Kids",
     type: "Ground Crackers",
-    image: "https://images.unsplash.com/photo-1482575832494-771f74bf6857?w=400"
+    image: "https://www.fireworks2home.com/wp-content/uploads/2020/10/red-and-green-sparklers-ayyan.jpg"
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const products = [
     price: 200,
     category: "Kids",
     type: "Ground Crackers",
-    image: "https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?w=400"
+    image: "https://www.fireworks2home.com/wp-content/uploads/2020/10/1506510070_flower-pots-special-specialflower-pot-cornation.jpg"
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const products = [
     price: 180,
     category: "Kids",
     type: "Ground Crackers",
-    image: "https://images.unsplash.com/photo-1546484396-fb3fc6f95f98?w=400"
+    image: "https://cdn.dotpe.in/longtail/store-items/6997269/t5JlU6G8.webp"
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ export const products = [
     price: 300,
     category: "Adults",
     type: "Aerial Crackers",
-    image: "https://images.unsplash.com/photo-1484417894907-623942c8ee29?w=400"
+    image: "https://m.media-amazon.com/images/I/71s-j9nrQHL._UF1000,1000_QL80_.jpg"
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ export const products = [
     price: 500,
     category: "Adults",
     type: "Sound Crackers",
-    image: "https://images.unsplash.com/photo-1467810563316-b5476525c0f9?w=400"
+    image: "https://www.kaliswari-fireworks.com/img/products/atombom/1.jpg"
   },
   {
     id: 6,
@@ -45,7 +45,7 @@ export const products = [
     price: 800,
     category: "Adults",
     type: "Sound Crackers",
-    image: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?w=400"
+    image: "https://www.bbassets.com/media/uploads/p/l/40116443_1-standard-crackers-atom-bombs-thunder-bomb-green.jpg"
   },
   {
     id: 7,
@@ -53,7 +53,7 @@ export const products = [
     price: 250,
     category: "Kids",
     type: "Ground Crackers",
-    image: "https://images.unsplash.com/photo-1513151233558-d860c5398176?w=400"
+    image: "https://mmtoyworld.com/cdn/shop/files/Untitled_design_67.jpg?v=1732350996"
   },
   {
     id: 8,
@@ -61,7 +61,7 @@ export const products = [
     price: 600,
     category: "Adults",
     type: "Aerial Crackers",
-    image: "https://images.unsplash.com/photo-1532635211-c96f0dc82d7e?w=400"
+    image: "https://dpmartcloud.s3.ap-south-1.amazonaws.com/images/66e50e095e6c8.webp?p=full"
   },
   {
     id: 9,
@@ -69,7 +69,7 @@ export const products = [
     price: 100,
     category: "Kids",
     type: "Electric Crackers",
-    image: "https://images.unsplash.com/photo-1514316454349-750a7fd3da3a?w=400"
+    image: "https://www.crackersindia.com/wp-content/uploads/2019/01/5000_wala-500x500.jpg"
   },
   {
     id: 10,
@@ -77,7 +77,7 @@ export const products = [
     price: 450,
     category: "Adults",
     type: "Sound Crackers",
-    image: "https://images.unsplash.com/photo-1481480142250-ca85c611921e?w=400"
+    image: "https://www.srisoftwares.com/sriarunagiriagencies.com/images/larcrackers/5000-lar-crackers.jpg"
   },
   {
     id: 11,
@@ -85,7 +85,7 @@ export const products = [
     price: 350,
     category: "Kids",
     type: "Ground Crackers",
-    image: "https://images.unsplash.com/photo-1502481851512-e9e2529bfbf9?w=400"
+    image: "https://gocracker.in/wp-content/uploads/2024/01/WONDER-3-IN-1.jpg"
   },
   {
     id: 12,
@@ -93,6 +93,6 @@ export const products = [
     price: 900,
     category: "Adults",
     type: "Aerial Crackers",
-    image: "https://images.unsplash.com/photo-1513151233558-d860c5398176?w=400"
+    image: "https://www.fireworks2home.com/wp-content/uploads/2020/10/holi-night-120-multi-color-shots-mercury.jpg"
   }
 ];
